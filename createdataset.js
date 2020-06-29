5a574fd222aa8987e8ef486214d9787d61c0f132
Tpierga <thibaultpierga@outlook.fr>
2020-06-29 20:39:32 +0200

    final project

--- createdataset.js
+++ createdataset.js
@@ -1,88 +1,35 @@
 const R = require('ramda')
 const ObjectsToCsv = require('objects-to-csv');
 
-const truck_speed = 0.025  //miles per seconds
-const capacity = 50
 
-arrayDataset = []
 
-/**
- *
- * model of the dict object with keys
-oneOfferPrev =
-    {
-        pricePerBox:0,
-        boxes:0,
-        travel:0,
-        timeSinceLast: 0,
-        valueOfCourse: 0, // total value generated if offer accepted
-        valuePerMile:0, // ratio of value
-        distanceTruck1:0, // distance the truck has to cover before course end
-        distanceTruck2:0,
-        distanceTruck3:0,
-        distanceTruck4:0,
-        distanceTruck5:0,
-        takeCommand: 0 // it is 1 if the offer was accepted
-    }
- **/
 
-//first offer being placed
-const oneOffer= {
-    pricePerBox: Math.ceil(Math.random() * 10),
-    boxes: Math.floor(Math.random() * 100),
-    travel: Math.floor(Math.random() * 1000),
-    timeSinceLast: Math.floor(Math.random() * 10000)
-}
 
-// calculating the number of trucks needed to transport the boxes, and the value generated
-let nb_cargos = Math.floor(oneOffer['boxes']/capacity) + 1;
-oneOffer['valueOfCourse']=0
-for(i=0; i<nb_cargos-1; i++)
-{
-    oneOffer['valueOfCourse'] += capacity * oneOffer['pricePerBox'] //quotient
-}
-oneOffer['valueOfCourse']+= oneOffer['boxes']%capacity * oneOffer['pricePerBox'] // remainder
-oneOffer['valuePerMile'] = oneOffer['valueOfCourse'] / oneOffer['travel']
 
-let listTrucks = []
-oneOffer['distanceTruck1'] = 0;
-listTrucks.push(oneOffer['distanceTruck1'])
-oneOffer['distanceTruck2'] = 0;
-listTrucks.push(oneOffer['distanceTruck2'])
-oneOffer['distanceTruck3'] = 0;
-listTrucks.push(oneOffer['distanceTruck3'])
-oneOffer['distanceTruck4'] = 0;
-listTrucks.push(oneOffer['distanceTruck4'])
-oneOffer['distanceTruck5'] = 0;
-listTrucks.push(oneOffer['distanceTruck5'])
+function createDatasetAndReturnAverage(){
 
-oneOffer['takeCommand'] = 1;
-nb_truck_loaded = 0
-for (i = 0; i<listTrucks.length; i++)
-{
-    if (listTrucks[i]==0)
-    {
-        oneOffer['distanceTruck'+(i+1).toString()]=oneOffer['travel'];
-        nb_truck_loaded +=1;
-        if (nb_truck_loaded==nb_cargos)
+    oneOfferPrev =
         {
-            break;
+            pricePerBox:0,
+            boxes:0,
+            travel:0,
+            timeSinceLast: 0,
+            valueOfCourse: 0, // total value generated if offer accepted
+            valuePerMile:0, // ratio of value
+            distanceTruck1:0, // distance the truck has to cover before course end
+            distanceTruck2:0,
+            distanceTruck3:0,
+            distanceTruck4:0,
+            distanceTruck5:0,
+            takeCommand: 0 // it is 1 if the offer was accepted
         }
-    }
-}
-// end of first offer
 
-console.log(oneOffer);
 
-oneOfferPrev = oneOffer;
-arrayDataset.push(oneOffer);
-let t = 1;
-let somme = oneOfferPrev['valuePerMile'];
-let benef = oneOfferPrev['valueOfCourse'];
+    const truck_speed = 0.025  //miles per seconds
+    const capacity = 50
 
-while (t<500)
-{
-
+    let arrayDataset = []
+    //first offer being placed
     const oneOffer= {
         pricePerBox: Math.ceil(Math.random() * 10),
         boxes: Math.floor(Math.random() * 100),
@@ -90,6 +37,7 @@ while (t<500)
         timeSinceLast: Math.floor(Math.random() * 10000)
     }
 
+// calculating the number of trucks needed to transport the boxes, and the value generated
     let nb_cargos = Math.floor(oneOffer['boxes']/capacity) + 1;
     oneOffer['valueOfCourse']=0
     for(i=0; i<nb_cargos-1; i++)
@@ -99,78 +47,122 @@ while (t<500)
     oneOffer['valueOfCourse']+= oneOffer['boxes']%capacity * oneOffer['pricePerBox'] // remainder
     oneOffer['valuePerMile'] = oneOffer['valueOfCourse'] / oneOffer['travel']
 
-    let distance_covered = oneOffer["timeSinceLast"] * truck_speed ;
-    console.log('distance covered : ' + distance_covered.toString());
-
     let listTrucks = []
-    oneOfferPrev['distanceTruck1'] > distance_covered ? oneOffer['distanceTruck1'] = Math.floor(oneOfferPrev['distanceTruck1'] - distance_covered): oneOffer['distanceTruck1']=0;
+    oneOffer['distanceTruck1'] = 0;
     listTrucks.push(oneOffer['distanceTruck1'])
-    oneOfferPrev['distanceTruck2'] > distance_covered ? oneOffer['distanceTruck2'] = Math.floor(oneOfferPrev['distanceTruck2'] - distance_covered): oneOffer['distanceTruck2']=0;
+    oneOffer['distanceTruck2'] = 0;
     listTrucks.push(oneOffer['distanceTruck2'])
-    oneOfferPrev['distanceTruck3'] > distance_covered ? oneOffer['distanceTruck3'] = Math.floor(oneOfferPrev['distanceTruck3'] - distance_covered): oneOffer['distanceTruck3']=0;
+    oneOffer['distanceTruck3'] = 0;
     listTrucks.push(oneOffer['distanceTruck3'])
-    oneOfferPrev['distanceTruck4'] > distance_covered ? oneOffer['distanceTruck4'] = Math.floor(oneOfferPrev['distanceTruck4'] - distance_covered): oneOffer['distanceTruck4']=0;
+    oneOffer['distanceTruck4'] = 0;
     listTrucks.push(oneOffer['distanceTruck4'])
-    oneOfferPrev['distanceTruck5'] > distance_covered ? oneOffer['distanceTruck5'] = Math.floor(oneOfferPrev['distanceTruck5'] - distance_covered): oneOffer['distanceTruck5']=0;
+    oneOffer['distanceTruck5'] = 0;
     listTrucks.push(oneOffer['distanceTruck5'])
 
-    console.log('value per Mile : ', oneOffer['valuePerMile']);
-    if (oneOffer['valuePerMile']!==Infinity)
+    oneOffer['takeCommand'] = 1;
+    nb_truck_loaded = 0
+    for (i = 0; i<listTrucks.length; i++)
     {
-        t+=1;
-        somme= somme + oneOffer['valuePerMile'];
-        averageValue = somme/t;
-        console.log("average value per mile : " + averageValue.toString());
-    }
-    else
-    {
-        console.log("value was Infinity");
-    }
-
-
-
-
-    const whoIsParked = c => c==0 ? 1:0;
-    let nb_trucks_ready = R.reduce((a,b) => a + b, 0, R.map(whoIsParked, listTrucks));
-
-    if (oneOffer['valuePerMile']>=averageValue/2 && nb_trucks_ready>=nb_cargos)
-    {
-        oneOffer['takeCommand'] = 1;
-        nb_truck_loaded = 0
-        for (i = 0; i<listTrucks.length; i++)
+        if (listTrucks[i]==0)
         {
-            if (listTrucks[i]==0)
+            oneOffer['distanceTruck'+(i+1).toString()]=oneOffer['travel'];
+            nb_truck_loaded +=1;
+            if (nb_truck_loaded==nb_cargos)
             {
-                oneOffer['distanceTruck'+(i+1).toString()]=oneOffer['travel'];
-                nb_truck_loaded +=1;
-                if (nb_truck_loaded==nb_cargos)
-                {
-                    break;
-                }
+                break;
             }
         }
     }
-    else
-    {
-        oneOffer['takeCommand'] = 0;
-    }
+    //end of the first offer
 
-    console.log(oneOffer);
-    oneOfferPrev = oneOffer;
-    if (oneOffer['takeCommand']===1)
-    {
-        benef += oneOffer['valueOfCourse']; // TODO should be added only when truck returns
-    }
-    console.log("total benef : " + benef.toString()+"\n\n");
+    prevOffer = oneOffer;
     arrayDataset.push(oneOffer);
+    let t = 1;
+    let somme = oneOfferPrev['valuePerMile'];
+    let benef = oneOfferPrev['valueOfCourse'];
+
+    while (t<5000)
+    {
+
+        const oneOffer= {
+            pricePerBox: Math.ceil(Math.random() * 10),
+            boxes: Math.floor(Math.random() * 100),
+            travel: Math.floor(Math.random() * 1000),
+            timeSinceLast: Math.floor(Math.random() * 10000)
+        }
+
+        let nb_cargos = Math.floor(oneOffer['boxes']/capacity) + 1;
+        oneOffer['valueOfCourse']=0
+        for(i=0; i<nb_cargos-1; i++)
+        {
+            oneOffer['valueOfCourse'] += capacity * oneOffer['pricePerBox'] //quotient
+        }
+        oneOffer['valueOfCourse']+= oneOffer['boxes']%capacity * oneOffer['pricePerBox'] // remainder
+        oneOffer['valuePerMile'] = oneOffer['valueOfCourse'] / oneOffer['travel']
+
+        let distance_covered = oneOffer["timeSinceLast"] * truck_speed ;
+
+
+        // check if the trucks have finished there travel and update distance remaining
+        let listTrucks = []
+        oneOfferPrev['distanceTruck1'] > distance_covered ? oneOffer['distanceTruck1'] = Math.floor(oneOfferPrev['distanceTruck1'] - distance_covered): oneOffer['distanceTruck1']=0;
+        listTrucks.push(oneOffer['distanceTruck1'])
+        oneOfferPrev['distanceTruck2'] > distance_covered ? oneOffer['distanceTruck2'] = Math.floor(oneOfferPrev['distanceTruck2'] - distance_covered): oneOffer['distanceTruck2']=0;
+        listTrucks.push(oneOffer['distanceTruck2'])
+        oneOfferPrev['distanceTruck3'] > distance_covered ? oneOffer['distanceTruck3'] = Math.floor(oneOfferPrev['distanceTruck3'] - distance_covered): oneOffer['distanceTruck3']=0;
+        listTrucks.push(oneOffer['distanceTruck3'])
+        oneOfferPrev['distanceTruck4'] > distance_covered ? oneOffer['distanceTruck4'] = Math.floor(oneOfferPrev['distanceTruck4'] - distance_covered): oneOffer['distanceTruck4']=0;
+        listTrucks.push(oneOffer['distanceTruck4'])
+        oneOfferPrev['distanceTruck5'] > distance_covered ? oneOffer['distanceTruck5'] = Math.floor(oneOfferPrev['distanceTruck5'] - distance_covered): oneOffer['distanceTruck5']=0;
+        listTrucks.push(oneOffer['distanceTruck5'])
+
+        if (oneOffer['valuePerMile']!==Infinity)
+        {
+            t+=1;
+            somme= somme + oneOffer['valuePerMile'];
+            averageValue = somme/t;
+
+        }
+
+
+        const whoIsParked = c => c==0 ? 1:0;
+
+        // calculating the numbers of trucks remaining
+        let nb_trucks_ready = R.reduce((a,b) => a + b, 0, R.map(whoIsParked, listTrucks));
+
+        if (oneOffer['valuePerMile']>=averageValue/2 && nb_trucks_ready>=nb_cargos)
+        {
+            oneOffer['takeCommand'] = 1;
+            nb_truck_loaded = 0
+            for (i = 0; i<listTrucks.length; i++)
+            {
+                if (listTrucks[i]==0)
+                {
+                    oneOffer['distanceTruck'+(i+1).toString()]=oneOffer['travel'];
+                    nb_truck_loaded +=1;
+                    if (nb_truck_loaded==nb_cargos)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            oneOffer['takeCommand'] = 0;
+        }
+
+        oneOfferPrev = oneOffer;
+
+    }
+
+    (async () => {
+        const csv = new ObjectsToCsv(arrayDataset);
+        await csv.toDisk('./offers.csv');
+    })();
+    return averageValue;
 }
-
-(async () => {
-    const csv = new ObjectsToCsv(arrayDataset);
-    await csv.toDisk('./offers.csv');
-})();
-
-
+module.exports = {createDatasetAndReturnAverage};
 
 //automatically decide for each line if you send truck or not, by actuallising the mean of gain in file
 
--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const offers = require('./async-js');
 const R = require('ramda');
 const {prepareData, trainNN} = require('./nn');
 const {filterEmptyLines, readAndParseFile} = require('./parser');
+const {createDatasetAndReturnAverage} = require('./createdataset');
 //this is our listener. the functions should be called here.
 //start with parsing file
+let benef = 0;
 
+async function sendTruck(value, travelTime) {
+        let promise = new Promise((resolve, reject) => {
+                setTimeout(() => resolve(value), travelTime)
+        });
+        let amountMoney = await promise;
+        benef = benef+amountMoney;
+        console.log("Total benef :"+benef.toString());
+
+}
 
 let listoffer = [];
 let prevTime = Date.now();
@@ -13,8 +24,35 @@ let prevTime = Date.now();
 const capacity = 50
 
 // the speed is the number of miles covered by seconds
-const truck_speed = 0.025
+const truckSpeed = 0.025
 
+const nbTrucks = 5;
+
+const profitable = createDatasetAndReturnAverage()/2;
+
+console.log("value above which a course is considered profitable :"+profitable.toString());
+
+
+prevOffer =
+    {
+            pricePerBox:0,
+            boxes:0,
+            travel:0,
+            timeSinceLast: 0,
+            valueOfCourse: 0, // total value generated if offer accepted
+            valuePerMile:0 // ratio of value
+
+    }
+var i;
+for (i=0;i<nbTrucks; i++)
+{
+        prevOffer['distanceTruck'+i.toString()]=0
+}
+
+
+
+
+// This is the event receiver waiting for offers to arrive
 offers.marketEvent.on('New Offer', (offer) => {
 
         let currTime = Date.now()
@@ -30,33 +68,47 @@ offers.marketEvent.on('New Offer', (offer) => {
 
         offer['valuePerMile'] = offer['valueOfCourse'] / offer['travel']
 
-        let distance_covered = offer["timeSinceLast"] * truck_speed ;
+        let distance_covered = offer["timeSinceLast"] * truckSpeed ;
         console.log('distance covered : ' + distance_covered.toString());
+
+        let listTrucks = []
+        var i;
+        for (i = 0; i<nbTrucks; i++) {
+                // check if the trucks have finished there travel and update distance remaining
+
+                prevOffer['distanceTruck' + i.toString()] > distance_covered ? offer['distanceTruck'+i.toString()] = Math.floor(prevOffer['distanceTruck'+i.toString()] - distance_covered): offer['distanceTruck'+i.toString()]=0;
+                listTrucks.push(offer['distanceTruck'+i.toString()])
+        }
+
+        const whoIsParked = c => c==0 ? 1:0;
+
+        // calculating the numbers of trucks remaining
+        let nb_trucks_ready = R.reduce((a,b) => a + b, 0, R.map(whoIsParked, listTrucks));
+
+        // here we decide if it is possible to accept the offer and if it is, is it a good choice to accept it ?
+        if (offer['valuePerMile']>=profitable && nb_trucks_ready>=nb_cargos)
+        {
+                offer['takeCommand'] = 1;
+                for (i = 0; i <= nb_cargos-1; i++) {
+                        if (listTrucks[i] == 0) {
+                                offer['distanceTruck' + i.toString()] = offer['travel'];
+
+                        }
+                }
+        sendTruck(offer['valueOfCourse'], (offer['travel']/truckSpeed));
+
+        }
+        else {
+                offer['takeCommand'] = 0;
+        }
 
         R.pipe(R.append(offer), R.tap(console.log))(listoffer);
         prevOffer = offer;
         prevTime = currTime;
-
-
 });
 
 offers.launchEmitter();
 
-
-let offerTest =
-    {
-        pricePerBox: 7,
-        boxes: 21,
-        travel: 956,
-        timeSinceLast: 815,
-        valueOfCourse: 147,
-        valuePerMile: 0.15376569037656904,
-        distanceTruck1: 15,
-        distanceTruck2: 15,
-        distanceTruck3: 0,
-        distanceTruck4: 0,
-        distanceTruck5: 0
-    }
 
 //testData = prepareData(offerTest);
 
